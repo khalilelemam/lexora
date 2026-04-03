4829af9a46d642b5b8fd9f38b71eec54b2ea403b
Khalil Elemam <khalil.l.elemam@gmail.com>
2026-04-03 17:41:46 +0200

    chore(repo): remove noisy dump and merge artifact files

--- test_pipeline.py
+++
@@ -1,6 +0,0 @@
-import sys
-import os
-sys.path.append("/home/omran-xy/Workspace/Lexplore/eglex/ml-service")
-from app.schemas.prediction import PipelineMetrics, to_camel
-pm = PipelineMetrics(feature_extraction_ms=1.1, **{"total_fixations": 45, "mean_saccade_amplitude": 12.0})
-print(pm.model_dump(by_alias=True))
