3696db718e4616a4f2c066766b977012cc827e0a
Khalil Elemam <khalil.l.elemam@gmail.com>
2026-04-23 23:27:19 +0200

    fix(web): remove dangling export of CalibrationCanvasModeProps

--- web/src/features/test/components/calibration-modes/index.ts
+++ web/src/features/test/components/calibration-modes/index.ts
@@ -3,4 +3,4 @@ export { StickmanModeView } from './stickman-mode-view';
 export { StarModeView } from './star-mode-view';
 export { StickmanCanvas } from './stickman/stickman-canvas';
 export { StarCanvas } from './star/star-canvas';
-export type { CalibrationModeViewProps, CalibrationCanvasModeProps } from './types';
+export type { CalibrationModeViewProps } from './types';
